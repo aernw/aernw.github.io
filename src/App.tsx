a6c48bdd244cbedcfc892f3c25036467f0b9a794
erwan <erwan.seytor@epitech.eu>
2026-03-31 22:18:40 +0200

    feat: button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Button } from './components/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <section id="center">
@@ -16,17 +14,12 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>My first react app</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <Button>idk</Button>
       </section>
 
       <div className="ticks"></div>
