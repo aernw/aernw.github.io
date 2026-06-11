c86972b36edf3673b52fb7918263461ac5f3a120
erwan <erwan.seytor@epitech.eu>
2026-06-11 14:17:39 +0200

    feat: implement basic App component in index.tsx and clean up vite configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
 
+const App = () => {
+    return (
+        <div>Hello hi </div>
+    );
+};
+
 const domNode = document.getElementById("root") as HTMLElement;
 const root = createRoot(domNode);
 
